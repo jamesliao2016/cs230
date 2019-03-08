015bd8c93ce8050f28fa5ea66612c1b5c228d76e
Ryan Almodovar <ralmodovar@salesforce.com>
2019-03-07 20:15:40 -0800

    move bert data
    Former-commit-id: b282957658fc5a47528ab335268d3afc47413b3f

--- final/mains/bert_fine_tune.sh
+++ final/mains/bert_fine_tune.sh
@@ -1,7 +1,7 @@
 # Based on bert readme
 
 export BERT_HOME_DIR=../bert
-export BERT_BASE_DIR=$BERT_HOME_DIR/uncased_L-12_H-768_A-12
+export BERT_BASE_DIR=../bert_data/uncased_L-12_H-768_A-12
 export GLUE_DIR=../glue_data
 
 python $BERT_HOME_DIR/run_classifier.py \
