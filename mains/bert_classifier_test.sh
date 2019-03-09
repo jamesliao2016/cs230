af9ac9aff0998ab9fd9a79b31fbb89870057ae75
Ryan Almodovar <ralmodovar@salesforce.com>
2019-03-09 01:57:02 -0800

    add bert_classifier_test.sh
    Former-commit-id: f01ec1839ac6234b944f6fd2fa4474ce00f169ae

---
+++ mains/bert_classifier_test.sh
@@ -0,0 +1,22 @@
+# Based on bert readme
+
+export BERT_HOME_DIR=../bert
+export BERT_BASE_DIR=../bert_data/uncased_L-12_H-768_A-12
+export GLUE_DIR=../glue_data
+
+name="$1"
+
+if (( $# <= 0 )); then
+  echo 'Error: Provide task name arg'
+  exit 1
+fi
+
+python $BERT_HOME_DIR/run_classifier.py \
+  --task_name="$name" \
+  --do_predict=true \
+  --data_dir=$GLUE_DIR/"$name" \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$TRAINED_CLASSIFIER \
+  --max_seq_length=128 \
+  --output_dir=/tmp/"$name"_output/
--- mains/bert_classifier_train.sh
+++ mains/bert_classifier_train.sh
@@ -4,11 +4,17 @@ export BERT_HOME_DIR=../bert
 export BERT_BASE_DIR=../bert_data/uncased_L-12_H-768_A-12
 export GLUE_DIR=../glue_data
 
+name="$1"
+if (( $# <= 0 )); then
+  echo 'Error: Provide task name arg'
+  exit 1
+fi
+
 python $BERT_HOME_DIR/run_classifier.py \
-  --task_name=MRPC \
+  --task_name="$name" \
   --do_train=true \
   --do_eval=true \
-  --data_dir=$GLUE_DIR/MRPC \
+  --data_dir=$GLUE_DIR/"$name" \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
@@ -16,4 +22,4 @@ python $BERT_HOME_DIR/run_classifier.py \
   --train_batch_size=32 \
   --learning_rate=2e-5 \
   --num_train_epochs=3.0 \
-  --output_dir=/tmp/mrpc_output/
+  --output_dir=/tmp/"$name"_output/
