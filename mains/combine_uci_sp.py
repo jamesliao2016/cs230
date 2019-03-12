22271f4bc822edb7be0c239b1c164829e5432e29
Ryan Almodovar <ralmodovar@salesforce.com>
2019-03-12 01:31:26 -0700

    WIP algo notes
    Former-commit-id: f0b8e0e6cfea36bf6d13e078d5f986d46b931688

--- mains/combine_uci_sp.py
+++ mains/combine_uci_sp.py
@@ -1,44 +1,61 @@
 # Combine UCI news aggregator dataset with S&P 500 and DJIA close
 
 # Format:
-# Date,Title,Hostname,Category,SP_close,DJIA_close
+# Date,Title,Hostname,Category,SP_Close,DJIA_Close,Delta_prev,Delta_next
 
 import csv
 import datetime
 
 data_dir = '../data'
+news_file = f'{data_dir}/uci-news-aggregator.csv'
+sp_file = f'{data_dir}/SP_table.csv'
+djia_file = f'{data_dir}/DJIA_table.csv'
 
+whitelist = set('abcdefghijklmnopqrstuvwxyz 0123456789.,;\'-:?')
 
 def main():
-    with open(f'{data_dir}/uci-news-aggregator.csv') as csv_file:
+    with open(news_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
 
-        line_count = 0
-        for row in csv_reader:
-            if line_count > 10:
+        rows = ["date\ttitle\thostname\tcategory"]
+
+        for idx, row in enumerate(csv_reader):
+            if idx > 100:
                 break
 
-            if line_count == 0:
+            if idx == 0:
                 print(f'Column names are {", ".join(row)}')
-                line_count += 1
             else:
                 cleaned = process_news(row)
-                print(f'\t{cleaned}')
-                line_count += 1
+                rows.append(cleaned)
 
-    print(f'Processed {line_count} lines.')
+    write_output(rows)
+
+
+def write_output(rows, out_file=f'{data_dir}/result.tsv'):
+    with open(out_file, 'w') as f:
+        for r in enumerate(rows):
+            f.write("%s\n" % '\t'.join(r))
 
 
 def process_news(row):
     """Return format: date,title,hostname,category"""
 
     id, title, url, publisher, category, story, hostname, timestamp = row
-    return convert_time(timestamp), title, hostname, category
+    return convert_time(timestamp), normalize_headline(title), hostname, category
 
 
 def convert_time(timestamp):
-    s = timestamp / 1000.0
+    s = int(timestamp) / 1000
     return datetime.datetime.fromtimestamp(s).strftime('%Y-%m-%d')
+
+
+def normalize_headline(row):
+    result = row.lower()
+    # Delete useless character strings
+    result = result.replace('...', '')
+    result = ''.join(filter(whitelist.__contains__, result))
+    return result
 
 
 if __name__ == "__main__":
