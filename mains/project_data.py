7fcfb365f328750289d407ef7bb1f32f64fa9746
Ryan Almodovar <ralmodovar@salesforce.com>
2019-03-13 05:43:00 -0700

    fix project_data.py
    Former-commit-id: 3bf74027641753bb0b93ed651ba911db95ca1207

--- mains/project_data.py
+++ mains/project_data.py
@@ -1,31 +1,44 @@
-#!/usr/bin/env python
-# coding: utf-8
 
 # # Process data from sources
-
-# In[9]:
 
 
 import tensorflow as tf
 import tensorflow_hub as hub
-import matplotlib.pyplot as plt
 import numpy as np
-import os
-import pandas as pd
-import re
-import seaborn as sns
 import csv
 
 
 # ## Convert headlines to embeddings
 
-# In[6]:
+news_file = '../data/uci-news-aggregator.csv'
+# news_file = '../data/uci-news-aggregator_small.csv'
 
 
-news_file = 'data/uci-news-aggregator.csv'
+def main():
+    news = read_news()
+    headlines = [n[1] for n in news[1:]]
+
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder/2" #@param ["https://tfhub.dev/google/universal-sentence-encoder/2", "https://tfhub.dev/google/universal-sentence-encoder-large/3"]
+
+    # Import the Universal Sentence Encoder's TF Hub module
+    print('Getting Hub Module')
+    embed = hub.Module(module_url)
+    print('Finished getting Hub Module')
+
+    run_embed(embed, headlines)
 
 
-# In[14]:
+def run_embed(embed, headlines):
+    with tf.Session() as session:
+        session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+        headline_embeddings = session.run(embed(headlines))
+
+        for i, hemb in enumerate(np.array(headline_embeddings).tolist()):
+            print("Message: {}".format(headlines[i]))
+            print("Embedding size: {}".format(len(hemb)))
+            hemb_snippet = ", ".join(
+                (str(x) for x in hemb[:3]))
+            print("Embedding: [{}, ...]\n".format(hemb_snippet))
 
 
 def iter_csv(file_name, task_indexed):
@@ -35,51 +48,12 @@ def iter_csv(file_name, task_indexed):
             if idx != 0:
                 task_indexed(idx, row)
 
+
 def read_news():
     with open(news_file) as csv_file:
         return list(csv.reader(csv_file, delimiter=','))
 
 
-# In[17]:
-
-
-news = read_news()
-
-
-# In[22]:
-
-
-headlines = [n[1] for n in news[1:]][:10]
-
-headlines[:10]  # check
-
-
-# In[20]:
-
-
-module_url = "https://tfhub.dev/google/universal-sentence-encoder/2" #@param ["https://tfhub.dev/google/universal-sentence-encoder/2", "https://tfhub.dev/google/universal-sentence-encoder-large/3"]
-
-
-# In[24]:
-
-
-# Import the Universal Sentence Encoder's TF Hub module
-embed = hub.Module(module_url)
-
-with tf.Session() as session:
-  session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-  headline_embeddings = session.run(embed(headlines))
-
-  for i, hemb in enumerate(np.array(headline_embeddings).tolist()):
-    print("Message: {}".format(headlines[i]))
-    print("Embedding size: {}".format(len(hemb)))
-    hemb_snippet = ", ".join(
-        (str(x) for x in hemb[:3]))
-    print("Embedding: [{}, ...]\n".format(hemb_snippet))
-
-
-# In[ ]:
-
-
-
+if __name__ == "__main__":
+    main()
 
