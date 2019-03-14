39f7357658ce79bf21c605625b05c535171f1774
Ryan Almodovar <ralmodovar@salesforce.com>
2019-03-14 05:35:22 -0700

    WIP on gen data date
    Former-commit-id: f5cc4bd5c337ece66a101beb9a3aa2caa99e2080

--- mains/gen_market_embed.py
+++ mains/gen_market_embed.py
@@ -2,17 +2,34 @@
 # labels, deltas, embeddings (512)
 
 import csv
+import pandas
+import datetime
+
 
 def main():
 
-    with open('data/embedding_results.csv') as f:
-        embeddings = list(csv.reader(f, delimiter=','))
+    embeddings = pandas.read_csv('../data/embedding_results_small.csv')
+
+    djia = pandas.read_csv('../data/DJIA_2014.csv')
+    sp = pandas.read_csv('../data/SP_2014.csv')
+    djia_deltas = compute_deltas(djia)
+
 
     # small file
-    with open('data/market_headline_embeddings_small.tsv') as f:
+    with open('../data/market_headline_embeddings_small.tsv', 'w') as f:
+        f.write('{}\t{}\t{}'.format())
 
 
-def get_
+def compute_deltas(row, dataset, dateOffset=0):
+    date = datetime.strptime(row, '%Y-%m-%d')
+    date = date + datetime.timedelta(days=dateOffset)
+    row = date.strftime('%Y-%m-%d')
+    currentstockDay = dataset[dataset['Date'] == row]
+    if not currentstockDay.empty:
+        return currentstockDay.iloc[0]['Close'] > currentstockDay.iloc[0]['Open']
+    else:
+        return False
+
 
 if __name__ == '__main__':
     main()
