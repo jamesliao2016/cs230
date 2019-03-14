704a3b787cce5c69836174326898565c7dc79f9e
Ryan Almodovar <ralmodovar@salesforce.com>
2019-03-14 06:36:25 -0700

    WIP almost, date missing
    Former-commit-id: a829e88f2129c46a4ec0173af1f393d306fa828d

--- mains/gen_market_embed.py
+++ mains/gen_market_embed.py
@@ -6,19 +6,22 @@ import pandas as pd
 import datetime
 import numpy as np
 
-
 def main():
-    embeddings = pd.read_csv('../data/embedding_results_small.csv')
-
     djia = pd.read_csv('../data/DJIA_2014.csv', dtype={'Open': np.float32, 'Close': np.float32})
     sp = pd.read_csv('../data/SP_2014.csv', dtype={'Open': np.float32, 'Close': np.float32})
     djia_labels, djia_deltas = compute_label_delta(djia)
     sp_labels, sp_deltas = compute_label_delta(sp)
+    embeddings = pd.read_csv('../data/embedding_results_small.csv')
+
+    output = pd.concat([djia_labels, djia_deltas, sp_labels, sp_deltas, embeddings], axis=1)
+    output.to_csv('../data/market_headline_embeddings_small.csv',
+                  header=False,
+                  index=False)
 
 
 def compute_label_delta(stock):
     deltas = stock['Open'] - stock['Close']
-    labels = deltas.apply(lambda d: int(d > 0))
+    labels = deltas.copy().apply(lambda d: int(d > 0)).astype(int)
     return labels, deltas
 
 
