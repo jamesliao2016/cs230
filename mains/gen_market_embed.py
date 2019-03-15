4621a4bdf8fe6bad61d435ca89041e3ff78be99d
Ryan Almodovar <ralmodovar@salesforce.com>
2019-03-14 20:06:21 -0700

    WIP progress
    Former-commit-id: dfe51dd612e048a62757b6305824b87b651a8aa7

--- mains/gen_market_embed.py
+++ mains/gen_market_embed.py
@@ -1,21 +1,40 @@
-# Produces file called market_headline_embeddings_*.tsv
-# labels, deltas, embeddings (512)
 
 import csv
 import pandas as pd
 import datetime
 import numpy as np
+from mains.combine_data import DataCombiner
+
+data_dir = '../data'
+djia_file = '{}/DJIA_2014.csv'.format(data_dir)
+sp_file = '{}/SP_2014.csv'.format(data_dir)
+news_file = '{}/uci-news-aggregator_small.csv'.format(data_dir)
+
 
 def main():
-    djia = pd.read_csv('../data/DJIA_2014.csv', dtype={'Open': np.float32, 'Close': np.float32})
-    sp = pd.read_csv('../data/SP_2014.csv', dtype={'Open': np.float32, 'Close': np.float32})
+    '''
+    '''
+    # embeddings = pd.read_csv('../data/embedding_results_small.csv')
+    stock_dtype = {'Open': np.float32, 'Close': np.float32}
+    djia = pd.read_csv(djia_file, dtype=stock_dtype)
+    sp = pd.read_csv(sp_file, dtype=stock_dtype)
+
+    news = pd.read_csv(news_file)
+    res = news['TIMESTAMP'].apply(convert_time).to_frame().join(news['TITLE'])
+
+    existing_dates = set(djia['Date'].unique()).intersection(set(sp['Date'].unique()))
+
+    mask = res['TIMESTAMP'].isin(existing_dates)
+    valid_entries = res[mask]
+
+    # filter weekends (missing dates in djia/sp)
+
     djia_labels, djia_deltas = compute_label_delta(djia)
     sp_labels, sp_deltas = compute_label_delta(sp)
-    embeddings = pd.read_csv('../data/embedding_results_small.csv')
 
-    output = pd.concat([djia_labels, djia_deltas, sp_labels, sp_deltas, embeddings], axis=1)
+    output = pd.concat([djia_labels, djia_deltas, sp_labels, sp_deltas], axis=1)
     output.to_csv('../data/market_headline_embeddings_small.csv',
-                  header=False,
+                  header=['djia_label', 'djia_delta', 'sp_label', 'sp_delta'],
                   index=False)
 
 
@@ -23,6 +42,22 @@ def compute_label_delta(stock):
     deltas = stock['Open'] - stock['Close']
     labels = deltas.copy().apply(lambda d: int(d > 0)).astype(int)
     return labels, deltas
+
+
+def convert_time(timestamp):
+    s = int(timestamp) / 1000
+    return datetime.datetime.fromtimestamp(s).strftime('%Y-%m-%d')
+
+
+def __findStockChange(self, row, dataset, dateOffset=0):
+    date = datetime.strptime(row, '%Y-%m-%d')
+    date = date + datetime.timedelta(days=dateOffset)
+    row = date.strftime('%Y-%m-%d')
+    currentstockDay = dataset[dataset['Date'] == row]
+    if not currentstockDay.empty:
+        return currentstockDay.iloc[0]['Close'] > currentstockDay.iloc[0]['Open']
+    else:
+        return False
 
 
 if __name__ == '__main__':
