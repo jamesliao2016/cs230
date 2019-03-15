0558c16a989220d0b260392c509950304cdfed05
Ryan Almodovar <ralmodovar@salesforce.com>
2019-03-15 03:20:35 -0700

    tf import
    Former-commit-id: 1095b00b2c3bcb4a76b2cd7d6dd5b91f2e9199df

--- mains/kmodel.py
+++ mains/kmodel.py
@@ -1,6 +1,6 @@
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.callbacks import ModelCheckpoint, TensorBoard
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
 
 import numpy as np
 import pandas as pd
