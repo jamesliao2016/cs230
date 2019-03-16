c9d462d35d0298de563ee9b728c9f1f6c9c75760
Ryan Almodovar <ralmodovar@salesforce.com>
2019-03-15 20:40:05 -0700

    Refactor, use day offset, train dev test split
    Former-commit-id: 7a7a2482f57cb41bbeba3e96551502dc7f02e882

--- mains/kmodel.py
+++ mains/kmodel.py
@@ -2,42 +2,34 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import tensorflow_hub as hub
+import datetime as dt
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv1D, MaxPooling1D
 from keras.callbacks import ModelCheckpoint, TensorBoard
 
-# fix random seed for reproducibility
-np.random.seed(7)
-
 data_dir = '../data'
 dataset_file = '{}/combined_result.tsv'.format(data_dir)
 embeddings_file = '{}/embedding_results.csv'.format(data_dir)
 
-model_name = 'conv1d'
+model_name = 'day_offset_2'
 tb_log_dir = '../experiments/univ/{}'.format(model_name)
 
 
 def main():
     dataset = pd.read_table(dataset_file)
-    headlines = dataset['title'].values
-    labels = dataset['sp_label'].values
+    train_set, dev_set, test_set = split_train_dataset(dataset)
 
-    embeddings = load_embeddings(headlines)
+    # input
+    embeddings = load_embeddings(train_set['title'].values)
+
+    # output
+    labels = get_labels(dataset, label='sp_label', day_offset=2)
 
     # create model
     model = Sequential()
-    model.add(Dense(256, input_dim=512, activation='relu'))
-    model.add(Dropout(0.2))
-    # convolution layers
-    model.add(Conv1D(nb_filter=32,
-                     filter_length=4,
-                     border_mode='valid',
-                     activation='relu'))
-    model.add(MaxPooling1D(pool_length=2))
-    model.add(Dropout(0.2))
-    # output
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(256, input_dim=512, activation='relu', bias_initializer='zeros'))
+    model.add(Dense(1, activation='sigmoid', bias_initializer='zeros'))
 
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -50,11 +42,20 @@ def main():
     callbacks_list = [checkpoint, tb_callback]
 
     # Fit the model
-    model.fit(embeddings, labels, validation_split=0.2, epochs=200, batch_size=32, callbacks=callbacks_list)
+    model.fit(embeddings, labels, epochs=200, batch_size=32, callbacks=callbacks_list)
 
     # evaluate the model
-    scores = model.evaluate(embeddings, labels)
-    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    scores = model.evaluate(dev_set, labels)
+    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+
+
+def get_labels(dataset, label, day_offset=0):
+    labels = dataset[label].values
+
+    def day_delta(day):
+        return dt.strptime(day, '%Y-%m-%d') + dt.timedelta(days=day_offset)
+
+    return labels['date'].apply(day_delta)
 
 
 def load_embeddings(headlines):
@@ -66,7 +67,7 @@ def load_embeddings(headlines):
 
 
 def fetch_headline_embeddings(headlines):
-    module_url = "https://tfhub.dev/google/universal-sentence-encoder/2" #@param ["https://tfhub.dev/google/universal-sentence-encoder/2", "https://tfhub.dev/google/universal-sentence-encoder-large/3"]
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder/2"  # @param ["https://tfhub.dev/google/universal-sentence-encoder/2", "https://tfhub.dev/google/universal-sentence-encoder-large/3"]
 
     # Import the Universal Sentence Encoder's TF Hub module
     print('Getting Hub Module')
@@ -80,6 +81,18 @@ def run_embed(embed, headlines):
     with tf.Session() as session:
         session.run([tf.global_variables_initializer(), tf.tables_initializer()])
         return session.run(embed(headlines))
+
+
+def split_train_dataset(dataset):
+    shuffled = dataset.sample(frac=1, random_state=7)  # shuffles the ordering of filenames (deterministic given the chosen seed)
+
+    n_d = len(shuffled)
+    split_1 = int(0.8 * n_d)
+    split_2 = int(0.9 * n_d)
+    train = shuffled[:split_1]
+    dev = shuffled[split_1:split_2]
+    test = shuffled[split_2:]
+    return train, dev, test
 
 
 if __name__ == '__main__':
