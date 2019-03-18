04454165ea3727457a4424a8136946c852a938d5
Ryan Almodovar <ralmodovar@salesforce.com>
2019-03-18 14:32:17 -0700

    WIP refactoring train.py
    Former-commit-id: d52bdde8bca9587e41866925657f382aa43c6564

--- mains/train.py
+++ mains/train.py
@@ -3,23 +3,29 @@ import os
 import pandas as pd
 import tensorflow as tf
 import tensorflow_hub as hub
-import keras.backend as K
 import keras_metrics as km
+import argparse
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv1D, MaxPooling1D, Embedding, LSTM, Activation
 from keras.callbacks import ModelCheckpoint, TensorBoard
 from gensim.models.keyedvectors import KeyedVectors
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
 
-DEBUG = False
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_name', default='w2v',
+                    help="Name of the model")
+parser.add_argument('--day_offset', default=0,
+                    help="Offset the labels by given day num")
+parser.add_argument('--data_dir', default='../data',
+                    help="Directory where to find the data file")
 
-day_offset = 2
+args = parser.parse_args()
 
-data_dir = '../data'
-dataset_file = '{}/combined_result_day_offset_{}{}.tsv'.format(data_dir, day_offset, '_small' if DEBUG else '')
-
-model_name = 'w2v/day_offset_{}'.format(day_offset)
-tb_log_dir = '../experiments/{}'.format(model_name)
+dataset_file = '{}/combined_result_day_offset_{}.tsv'.format(args.data_dir, args.day_offset)
+tb_log_dir = '../experiments/{}'.format(args.model_name)
 
 
 def main():
@@ -34,13 +40,13 @@ def main():
     checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     tb_callback = TensorBoard(log_dir=tb_log_dir, histogram_freq=0, write_graph=True, write_images=True)
 
-    model, y_pred = create_model(headlines, embed_type='word')
+    model = create_model(headlines, embed_type='word')
 
     # Fit the model
-    model.fit(y_pred, labels, validation_split=0.2, epochs=200, batch_size=32, callbacks=[checkpoint, tb_callback])
+    model.fit(headlines, labels, validation_split=0.2, epochs=200, batch_size=32, callbacks=[checkpoint, tb_callback])
 
     # evaluate the model
-    scores = model.evaluate(y_pred, labels)
+    scores = model.evaluate(headlines, labels)
     print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
 
@@ -51,22 +57,64 @@ class WordModel:
 
 def create_model(headlines, embed_type='word'):
     def setup_word_model():
-        sentences = [[w for w in h.lower().split()] for h in headlines]
+        word_vectors = KeyedVectors.load_word2vec_format('../GoogleNews-vectors-negative300.bin', binary=True)
 
-        # TODO: google word2vec
-        # word_vectors = KeyedVectors.load_word2vec_format('../GoogleNews-vectors-negative300.bin', binary=True)
+        NUM_WORDS = 20000
+        tokenizer = Tokenizer(num_words=NUM_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n\'',
+                              lower=True)
+        tokenizer.fit_on_texts(texts)
+        sequences_train = tokenizer.texts_to_sequences(texts)
+        sequences_valid = tokenizer.texts_to_sequences(val_data.text)
+        word_index = tokenizer.word_index
 
-        word_model = gensim.models.Word2Vec(sentences, size=100, min_count=1, window=5, iter=100)
-        pretrained_weights = word_model.wv.syn0
-        vocab_size, emdedding_size = pretrained_weights.shape
+        EMBEDDING_DIM = 300
+        vocabulary_size = 20000
+        embedding_matrix = np.zeros((vocabulary_size, EMBEDDING_DIM))
+        for word, i in word_index.items():
+            if i >= vocabulary_size:
+                break
+            try:
+                embedding_vector = word_vectors[word]
+                embedding_matrix[i] = embedding_vector
+            except KeyError:
+                embedding_matrix[i] = np.random.normal(0, np.sqrt(0.25), EMBEDDING_DIM)
 
-        model = Sequential()
-        model.add(Embedding(input_dim=vocab_size, output_dim=emdedding_size, weights=[pretrained_weights]))
-        model.add(LSTM(units=emdedding_size))
-        model.add(Dense(units=vocab_size, activation='softmax'))
-        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy', km.binary_precision(), km.binary_recall()])
+        del (word_vectors)
 
-        return model, pretrained_weights
+        from keras.layers import Embedding
+        embedding_layer = Embedding(vocabulary_size,
+                                    EMBEDDING_DIM,
+                                    weights=[embedding_matrix],
+                                    trainable=True)
+        sequence_length = X_train.shape[1]
+        filter_sizes = [3, 4, 5]
+        num_filters = 100
+        drop = 0.5
+        inputs = Input(shape=(sequence_length,))
+        embedding = embedding_layer(inputs)
+        reshape = Reshape((sequence_length, EMBEDDING_DIM, 1))(embedding)
+
+        conv_0 = Conv2D(num_filters, (filter_sizes[0], EMBEDDING_DIM), activation='relu',
+                        kernel_regularizer=regularizers.l2(0.01))(reshape)
+        conv_1 = Conv2D(num_filters, (filter_sizes[1], EMBEDDING_DIM), activation='relu',
+                        kernel_regularizer=regularizers.l2(0.01))(reshape)
+        conv_2 = Conv2D(num_filters, (filter_sizes[2], EMBEDDING_DIM), activation='relu',
+                        kernel_regularizer=regularizers.l2(0.01))(reshape)
+
+        maxpool_0 = MaxPooling2D((sequence_length - filter_sizes[0] + 1, 1), strides=(1, 1))(conv_0)
+        maxpool_1 = MaxPooling2D((sequence_length - filter_sizes[1] + 1, 1), strides=(1, 1))(conv_1)
+        maxpool_2 = MaxPooling2D((sequence_length - filter_sizes[2] + 1, 1), strides=(1, 1))(conv_2)
+
+        merged_tensor = concatenate([maxpool_0, maxpool_1, maxpool_2], axis=1)
+        flatten = Flatten()(merged_tensor)
+        reshape = Reshape((3 * num_filters,))(flatten)
+        dropout = Dropout(drop)(flatten)
+        output = Dense(units=3, activation='softmax', kernel_regularizer=regularizers.l2(0.01))(dropout)
+
+        # this creates a model that includes
+        model = Model(inputs, output)
+
+        return model
 
     def setup_sentence_model():
         embeddings = get_sentence_embeddings(headlines)
@@ -77,12 +125,6 @@ def create_model(headlines, embed_type='word'):
         return model, embeddings
 
     return setup_word_model() if embed_type == 'word' else setup_sentence_model()
-
-
-def penalized_loss(noise):
-    def loss(y_true, y_pred):
-        return K.mean(K.square(y_pred - y_true) - K.square(y_true - noise), axis=-1)
-    return loss
 
 
 def get_sentence_embeddings(headlines):
